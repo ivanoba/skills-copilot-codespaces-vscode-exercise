4610107b96dc9bf8a96273c0ce12a4e303e0b497
ivanoba <ivanobajuarez@gmail.com>
2023-07-18 00:23:42 +0000

    Copilot third commmit

---
+++ comments.js
@@ -0,0 +1,74 @@
+// Create web server
+// 
+// Path: server.js
+// var http = require("http");
+// 
+// function onRequest(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+// }
+// 
+// http.createServer(onRequest).listen(8000);
+// 
+// Path: server.js
+// var http = require("http");
+// 
+// function onRequest(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<h1>Hello World</h1>");
+//     response.end();
+// }
+// 
+// http.createServer(onRequest).listen(8000);
+// 
+// Path: server.js
+// var http = require("http");
+// 
+// function onRequest(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<h1>Hello World</h1>");
+//     response.end();
+// }
+// 
+// http.createServer(onRequest).listen(8000);
+// 
+// Path: server.js
+// var http = require("http");
+// 
+// function onRequest(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<h1>Hello World</h1>");
+//     response.end();
+// }
+// 
+// http.createServer(onRequest).listen(8000);
+// 
+// Path: server.js
+// var http = require("http");
+// 
+// function onRequest(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<h1>Hello World</h1>");
+//     response.end();
+// }
+// 
+// http.createServer(onRequest).listen(8000);
+// 
+// Path: server.js
+// var http = require("http");
+// 
+// http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<h1>Hello World</h1>");
+//     response.end();
+// }).listen(8000);
+// 
+// Path: server.js
+// var http = require("http");
+// 
+// http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("<h1>Hello World</h1>");
+//     response.end();
+// }).listen(8000);
